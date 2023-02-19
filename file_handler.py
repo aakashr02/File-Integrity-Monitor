f404d98d1991ef0f031fe95d51d21b5664f49fbf
subu2003 <87696656+subu2003@users.noreply.github.com>
2023-02-19 21:09:28 +0530

    Add files via upload
    Version 2  -  Regex Feature Implemented, Added Menu Driven feature

--- file_handler.py
+++ file_handler.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
 Created on Fri Sep  6 20:46:33 2019
 file_handler.py
@@ -13,8 +11,8 @@ Handle file operations
 import os
 import pickle 
 import datetime
-
-
+from prettytable import PrettyTable
+myTable = PrettyTable(["Status","File Name","Time"])
 #==============================================================================
 # Recursively walks the directory tree
 # Finds directories and files
@@ -42,7 +40,7 @@ def search_directory_tree(root_dir, IGNORE_LIST, LOG_FILE):
         return directories
             
     except Exception as e:
-        log(LOG_FILE, \
+        alertlog(LOG_FILE, \
             "Error while scanning directories and files")
         
 
@@ -63,7 +61,7 @@ def save_dict(dictionary, file, LOG_FILE):
         initial_scan_file.close
         
     except Exception as e:
-        log(LOG_FILE, \
+        alertlog(LOG_FILE, \
             "Error while saving the dictionary")
 
 
@@ -86,7 +84,7 @@ def load_dict(file, LOG_FILE):
         return loaded_dict
         
     except Exception as e:
-        log(LOG_FILE, \
+        alertlog(LOG_FILE, \
             "Error while loading the dictionary")
     
     
@@ -94,7 +92,7 @@ def load_dict(file, LOG_FILE):
 #==============================================================================
 # Log events
 #==============================================================================
-def log(log_dir, message):
+def applog(log_dir, message):
     
     # get time
     currentDT = datetime.datetime.now()
@@ -110,3 +108,27 @@ def log(log_dir, message):
                str(currentDT.strftime("%Y-%m-%d %H:%M:%S")) + \
                "\n")
     file.close
+
+def alertlog(log_dir, message,change,rootdir):
+    
+    # get time
+    currentDT = datetime.datetime.now()
+    
+    # log event
+    file = open(log_dir, "a+")
+    message=message+ \
+               "," + \
+               str(currentDT.strftime("%Y-%m-%d %H:%M:%S")+"\n")
+    file.write(message)
+    
+    if( change == "add"):
+        change+= "e"
+    print("File "+change+"d")
+    ls = message.split(',')
+    ls[1] = ls[1][len(str(rootdir)):]
+    myTable.add_row(ls)
+    file.close
+    
+def disptable():
+    print("\n\n------Summary of the Scan Session------\n")
+    print(myTable)
