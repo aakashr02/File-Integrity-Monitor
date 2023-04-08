d47ff5acbb574abe06a477e46b954ebc7967c892
Subramanian <87696656+subu2003@users.noreply.github.com>
2023-04-08 17:48:03 +0530

    Fixed Some Bugs and included some Comments in Code

--- regex.py
+++ regex.py
@@ -1,17 +1,23 @@
 from prettytable import PrettyTable
 import re
 import mail
+
+# function to search log records for the given regex
 def search_regex(regex,file,email):
     resultfile = open("result.log",'w')
+    
+    #Initializing variables to count the number of add/modify/delete satisfying the regex
     add=0
     change=0
     remove = 0
     myTable = PrettyTable(["Status", "File Name","Flag", "Time"])
     open_file = open(file,'r')
+    
+    #searching regex
     for f in open_file:
         match = re.search(regex,str(f))
         if(match):
-            # print(f.split(','))
+            #adding records to prettyTable formatter 
             myTable.add_row(f.split(','))
             resultfile.write(f)
             if(f[0:3]=="add"):
@@ -21,10 +27,17 @@ def search_regex(regex,file,email):
             else:
                 remove += 1
     open_file.close()
+    
+    # Displaying Results
     print("\n\n------Filtered Records------\n")
     print("Add : ",add,"\tChange : ",change,"\tRemove : ",remove,"\n")
     print(myTable)
     resultfile.close()
-    mail.mailing(email)
-    print("Email has been sent successfully!!!!!!!!!!!")
+    
+    #Mailing the Results
+    try:
+        mail.mailing(email)
+        print("Email has been sent successfully!!!!!!!!!!!")
+    except Exception as e:
+        print("Could not send mail")
     
